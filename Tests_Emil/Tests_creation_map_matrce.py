f77b86bf1432cde7b3899412ac068b19eeaafa64
Zeussurvival <emiljokui@gmail.com>
2025-12-07 02:43:52 +0100

    Pollution finis
    Tout est relie, jai regle des pbs pour le general en plus. Tao on top moi jdis

--- Tests_Emil/Tests_creation_map_matrce.py
+++ Tests_Emil/Tests_creation_map_matrce.py
@@ -22,45 +22,32 @@ clock = pygame.time.Clock()
 running = True
 dt = 0
 
-x_map = 20
-y_map = 20
+x_map = 30
+y_map = 30
 seed = 25
 
+def place_matrice_big_then_small(matrice,house_matrice,positions):
+    for true_y in range(house_matrice.shape[0]):
+        for true_x in range(house_matrice.shape[1]):
+            if 0 <= positions[1]+true_y < matrice.shape[1] and 0 <= positions[0]+true_x < matrice.shape[0]:
+                matrice[positions[1]+true_y,positions[0]+true_x] = house_matrice[true_y,true_x]
+    return matrice
+
+def place_matrice_big_then_small_addition(matrice,house_matrice,positions):
+    for true_y in range(house_matrice.shape[0]):
+        for true_x in range(house_matrice.shape[1]):
+            if 0 <= positions[1]+true_y < matrice.shape[1] and 0 <= positions[0]+true_x < matrice.shape[0]:
+                matrice[positions[1]+true_y,positions[0]+true_x] += house_matrice[true_y,true_x]
+    return matrice
 
 def creation_map_rectangle(width,height):
     Map = np.zeros((width,height), dtype=np.float32)
     return Map
 
-map123 = creation_map_rectangle(x_map,y_map)
-
-def create_random_pos(seed,numbers_to_gen,map_length):
-    random.seed(seed)
-    map_total = map_length[0] * map_length[1]
-    Liste_n = []
-    for _ in range(numbers_to_gen):
-        num = int(random.random()*map_total)
-        Liste_n.append((num // map_length[0],num % map_length[0]))
-    return Liste_n
-
-
-
-
-
-def pollution_creation_rond(Liste_pos,range_pollu,map): # pas forcement realiste au top mais bon
-    new_map = np.zeros(map.shape)
-    for pos in Liste_pos:
-        map_pollu = create_round_matrice(range_pollu)
-        # place_matrice_big_then_small(matrice,house_matrice,positions):
-        new_map = place_matrice_big_then_small(new_map,map_pollu,pos)
-        pass
-    return new_map
-
- 
 def create_round_matrice(radius):
     matrice = np.zeros((radius*2+1,radius*2+1))
     matrice[radius,radius] = 1
     matrice[radius,radius+2] = 1
-    print(matrice.shape[1])
     for x in range(matrice.shape[1]):
         distance_x = radius - x
         # print(distance_x)
@@ -72,14 +59,35 @@ def create_round_matrice(radius):
             # print(matrice[x,y])
     return matrice
 
+def create_random_pos(seed,numbers_to_gen,map_length):
+    random.seed(seed)
+    map_total = map_length[0] * map_length[1]
+    Liste_n = []
+    for _ in range(numbers_to_gen):
+        num = int(random.random()*map_total)
+        Liste_n.append((num // map_length[0],num % map_length[0]))
+    return Liste_n
+
+def pollution_creation_rond(Liste_pos,range_pollu,map): # pas forcement realiste au top mais bon
+    map_pollution = np.zeros(map.shape)
+    for pos in Liste_pos:
+        matrice_created = create_round_matrice(range_pollu)
+        map_pollution = place_matrice_big_then_small_addition(map_pollution,matrice_created,(pos[0]-range_pollu,pos[1]-range_pollu)) # place_matrice_big_then_small(matrice,house_matrice,positions)
+    print(Liste_pos)
+    print(map_pollution)
+    return map_pollution
+
+ 
+
+
 # mat = create_round_matrice(5)
 # for row in mat:
 #     print(row)
 
 
-def set_pollution_map_rectangle(number_of_origins,seed,map,range_pollu):
-    Liste_pos = create_random_pos(seed,number_of_origins,map.shape)
-    new_map = pollution_creation_rond(Liste_pos,range_pollu,map)
+def set_pollution_map_rectangle(number_of_pos,seed,map_actu,range_pollu):
+    Liste_pos = create_random_pos(seed,number_of_pos,map_actu.shape) # generation positions pr pollution
+    new_map = pollution_creation_rond(Liste_pos,range_pollu,map_actu) # creation de la vrai map de pollution
     # print(new_map)
     # print(Liste_pos)
     return new_map
@@ -90,8 +98,8 @@ def set_pollution_map_rectangle(number_of_origins,seed,map,range_pollu):
 
 
 
-
-
+map123 = creation_map_rectangle(x_map,y_map)
+map123_pollution = set_pollution_map_rectangle(5,seed,map123,5) # number_of_origins,seed,map,range_pollu
 
 
 
@@ -102,18 +110,13 @@ def set_pollution_map_rectangle(number_of_origins,seed,map,range_pollu):
 house_matrice = np.array([[3,4,5,6],
                          [7,8,9,18],
                          [17,16,15,14],])
-print(house_matrice)
-def place_matrice_big_then_small(matrice,house_matrice,positions):
-    for true_y in range(house_matrice.shape[0]):
-        for true_x in range(house_matrice.shape[1]):
-            print(positions[1]+true_x,matrice.shape[0])
-            if 0 < positions[1]+true_x < matrice.shape[0] and 0 < positions[0]+true_y < matrice.shape[1]:
-                matrice[positions[1]+true_x,positions[0]+true_y] = house_matrice[true_x,true_y]
-    
-    return matrice
-map123 = place_matrice_big_then_small(map123,house_matrice,(18,18))
 
-print(map123)
+
+map123 = place_matrice_big_then_small(map123,house_matrice,(13,10))
+
+# print(map123)
+# print(map123_pollution)
+
 # x = np.arange(36).reshape(6, 6)
 # print(x[4::-2,...])
 
