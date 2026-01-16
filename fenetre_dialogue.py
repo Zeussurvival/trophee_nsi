ee195116d6b3357a0f3f5335d2d8cf945cb01a68
Eudo08 <e2khovrine92@gmail.com>
2026-01-16 14:57:19 +0100

    Add custom font and typewriter effect to dialogue box
    Introduces a new TTF font for dialogue text and updates the dialogue box to use it. Implements a typewriter effect for displaying dialogue text character by character. Also refines input handling for showing and hiding the dialogue box.

--- fenetre_dialogue.py
+++ fenetre_dialogue.py
@@ -5,24 +5,32 @@ import os
 
 main_dir = os.path.split(os.path.abspath(__file__))[0]
 assets_dir = os.path.join(main_dir,"assets")
+police_dir = os.path.join(assets_dir,"polices")
 
-dialogue_image = pygame.image.load(os.path.join(assets_dir, "dialogue_box.png"))
 
 
 # pygame setup
 pygame.init()
+pygame.font.init()
 screen = pygame.display.set_mode((1280, 720))
 clock = pygame.time.Clock()
 running = True
 dt = 0
 objects = []
+timer = pygame.time.Clock()
+counter = 0
+speed = 3
+done = False
+
+dialogue_image = pygame.image.load(os.path.join(assets_dir, "dialogue_box.png"))
+police_dialogue_path = os.path.join(police_dir, "police_dialogue.ttf")
 
 
 dialogue_box_width = 400
 dialogue_box_height = 200
 dialogue_box_x = (screen.get_width() - dialogue_box_width) // 2
 dialogue_box_y = (screen.get_height() - dialogue_box_height) // 2
-dialogue_box = False
+dialogue_box = True
 
 player_pos = pygame.Vector2(screen.get_width() / 2, screen.get_height() / 2)
 
@@ -33,6 +41,8 @@ class Dialogue():
         self.y = y
         self.width = width
         self.height = height
+        self.dialogue_text = dialogue_text
+        self.snip = ""
         self.onclickFunction = onlickFunction
         self.one_press = one_press
         self.already_pressed = False
@@ -40,23 +50,28 @@ class Dialogue():
         self.dialogue_rect = pygame.Rect(0, 0, self.width, self.height)
         self.dialogue_rect.center = (x, y)
 
-
         self.normal_image = pygame.transform.scale(dialogue_image, (width, height))
         objects.append(self)
 
+        self.font = pygame.font.Font(police_dialogue_path, 25)
+        
     def draw(self, screen):
-    # Dessiner l'image de la boîte de dialogue
         screen.blit(self.normal_image, self.dialogue_rect)
+        text_surface = self.font.render(self.snip, True, (137, 244, 255))
+        text_rect = text_surface.get_rect(
+            topleft=(self.dialogue_rect.left + 40, self.dialogue_rect.top + 40))
+        screen.blit(text_surface, text_rect)
 
     def process(self):
-        if pygame.mouse.get_pressed():
-            if self.one_press :
-                self.onclickFunction()
-            elif not self.already_pressed:
-                self.onclickFunction()
-                self.already_pressed = True
-            else :
-                self.already_pressed = True
+        if pygame.mouse.get_pressed()[0]:
+            if self.dialogue_rect.collidepoint(pygame.mouse.get_pos()):
+                if self.one_press :
+                    self.onclickFunction()
+                elif not self.already_pressed:
+                    self.onclickFunction()
+                    self.already_pressed = True
+                else :
+                    self.already_pressed = True
         else :
             self.already_pressed = False
 
@@ -73,33 +88,47 @@ def open_dialogue_box ():
 def next():
     print("next")
 
-Dialogue( 650,600, 894, 200, "hello", next)
+dialogue_1 = Dialogue( 640,600, 894, 200, "hello tgretf regfeferhy", next)
 
 while running:
+
     # poll for events
     # pygame.QUIT event means the user clicked X to close your window
+    if counter < speed * len(dialogue_1.dialogue_text) :
+        counter +=1
+    elif counter >= speed * len(dialogue_1.dialogue_text):
+        done = True
+   
+    dialogue_1.snip = dialogue_1.dialogue_text[0:counter//speed]
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_ESCAPE :
+                if dialogue_box:
+                    dialogue_box = False
 
-    # fill the screen with a color to wipe away anything from last frame
+
     screen.fill("purple")
 
     pygame.draw.circle(screen, "red", player_pos, 40)
 
-    keys = pygame.key.get_pressed()
-    if keys[pygame.K_z]:
-        player_pos.y -= 300 * dt
-    if keys[pygame.K_s]:
-        player_pos.y += 300 * dt
-    if keys[pygame.K_q]:
-        player_pos.x -= 300 * dt
-    if keys[pygame.K_d]:
-        player_pos.x += 300 * dt
-
-    for object in objects:
-        object.process()
-        object.draw(screen)
+    if dialogue_box == False :
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_z]:
+            player_pos.y -= 300 * dt
+        if keys[pygame.K_s]:
+            player_pos.y += 300 * dt
+        if keys[pygame.K_q]:
+            player_pos.x -= 300 * dt
+        if keys[pygame.K_d]:
+            player_pos.x += 300 * dt
+    
+    if dialogue_box :
+        for object in objects:
+            object.process()
+            object.draw(screen)
 
     # flip() the display to put your work on screen
     pygame.display.flip()
