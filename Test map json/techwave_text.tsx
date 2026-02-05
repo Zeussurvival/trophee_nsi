d24361e5693499ab601a6449391612b87a3315b9
El!teW0lf <mateo@loslobos.fr>
2026-02-05 08:53:02 +0000

    update map (usine)

---
+++ Test map json/techwave_text.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="techwave_text" tilewidth="8" tileheight="8" tilecount="64" columns="8">
+ <image source="../../techwave_text.png" width="64" height="64"/>
+</tileset>
